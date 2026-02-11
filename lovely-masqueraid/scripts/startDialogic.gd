bbcd88e9252b20d4c901e748f99c6c5ae3169923
bobby <37113385+bobbyreed@users.noreply.github.com>
2026-02-11 12:12:20 -0600

    Hot fix 0.8.1 fixes breaking change  in choices when they have an audio file attached.

--- lovely-masqueraid/scripts/startDialogic.gd
+++ lovely-masqueraid/scripts/startDialogic.gd
@@ -1,6 +1,5 @@
 extends Node2D
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Dialogic.start('0menu')
@@ -9,3 +8,9 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+func _on_save_game_button_pressed() -> void:
+	Dialogic.Save.save("", false, Dialogic.Save.ThumbnailMode.NONE)
+
+func _on_load_game_button_pressed() -> void:
+	Dialogic.Save.load()
